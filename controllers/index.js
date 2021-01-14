70ae00d03dff769e5189bbeec04463553104acb9
jsRuiner <mega.vzlomer111@gmail.com>
2021-01-14 23:32:35 +0200

    Create posts.js, modify index.js and posts.js with posts function

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,11 @@
 const Router = require('koa-router');
 
 const auth = require('./auth');
+const posts = require('./posts');
 
 const router = new Router().prefix('/api');
+
+router.use(auth, posts);
 
 router.use(auth);
 
