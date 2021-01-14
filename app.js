2604c8c2d0b1426f56a5f7c82c6878f1e18af5ff
jsRuiner <mega.vzlomer111@gmail.com>
2021-01-14 20:42:57 +0200

    Create auth, index.js files, modife config, mongoose-config and app.js files, add registration and login form

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 
 const Koa = require('koa');
 
+//Export files
 const config = require('./lib/config');
 const handlers = require('./handlers');
+const controllers = require('./controllers');
 const mongooseConfig = require('./lib/mongoose-config');
 
 const app = new Koa();
@@ -15,6 +17,9 @@ const app = new Koa();
 handlers.forEach(h => app.use(h));
 mongooseConfig();
 
+app.use(controllers.routes());
+app.use(controllers.allowedMethods());
+
+
 app.listen(config.port,
   () => console.log(`Server has been started on port ${config.port}`));
-
---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Router = require('koa-router');
+
+const auth = require('./auth');
+
+const router = new Router().prefix('/api');
+
+router.use(auth);
+
+module.exports = router;
+
