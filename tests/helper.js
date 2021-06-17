6bb4c3c93784f5d5cd8787e93498bf4640551572
jsRuiner <mega.vzlomer111@gmail.com>
2021-06-17 19:40:54 +0300

    Add test-running files and mocha deps

---
+++ tests/helper.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const request = require('request-promise');
+
+const config = require('../lib/config');
+
+module.exports = {
+  request: request.defaults({
+    resolveWithFullResponse: true,
+    simple: false,
+    baseUrl: `http://localhost:${config.port}/api`
+  })
+};
